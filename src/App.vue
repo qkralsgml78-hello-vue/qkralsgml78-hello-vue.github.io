12368bc2c744cbc817949e365d93c4b55506d7c8
qkralsgml78 <qkralsgml78@gmail.com>
2024-01-31 15:12:24 +0900

    vue json 상하차 해보기
    - [ ] assets/category.json 만들기
    - [ ] src/App.vue 수정

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+import category from "@/assets/category.json"
+
+const categories = ref(category)
+//const parentMessage = ref('Parent')
+//const items = ref([{ message: 'Foo' }, { message: 'Bar'}])
+</script>
+
+<template>
+  <li v-for= "(item) in categories">
+     {{ item.name }} {{ item.code}}
+  </li>
+</template>
