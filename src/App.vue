403f0e9d35310907c268e57b28771b083742e522
qkralsgml78 <qkralsgml78@gmail.com>
2024-01-31 12:21:52 +0900

    basic

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const awesome = ref(true)
+</script>
+
+<template>
+<button @click="awesome = !awesome">toggle</button>
+
+    <h1 v-if="awesome">Vue is awesome!</h1>
+    <h1 v-else>Oh no 😥 </h1>
+
+    <h4 v-if = "awesome">TRUE</h4>
+    <h4 v-show = "awesome">TRUE</h4>
+</template>
+
