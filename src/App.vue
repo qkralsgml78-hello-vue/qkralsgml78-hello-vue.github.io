07a60d6a628e26f3d802ffb7246ce9626c87c817
qkralsgml78 <qkralsgml78@gmail.com>
2024-01-31 12:09:18 +0900

    src/App.vue 수정

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,9 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-
-//reactive state
-const count = ref({
-	cnt: 0,
-	name: "Park",
-	age: 20
-})//초기값을 1로 세팅
-
-// functions that mutate state and trigger updates
-function increment(){ //반응형 선택 함수
-    count.value.cnt++
-    count.value.name = count.value.name.split('').reverse().join('')
-    count.value.age = count.value.age + 3
-}
-
-//lifecycle hooks
-onMounted(() => console.log(`Theinitial count is ${count.value}.`))
+import { ref } from 'vue'
+const message = ref('hello world!')
 </script>
 
-
 <template>
-    <button @click="increment">Count is : {{ count.cnt }} {{ count.name }} {{ count.age }} </button>
+    <h1>{{ message }}</h1>
 </template>
-
-<style scoped>
-button {
-    font-weight: bold;
-}
-</style>
 
