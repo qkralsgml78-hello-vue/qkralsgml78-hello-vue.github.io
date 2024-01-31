2f0a9d3ded50b1c03f5f43228cdfa33dbbae9211
qkralsgml78 <qkralsgml78@gmail.com>
2024-01-31 13:35:21 +0900

    basic2

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <script setup>
 import { ref } from 'vue'
+//import { toggleAwesome } from '<my_package>'
 
 const awesome = ref(true)
+
+function toggleAwesome(v){
+    awesome.value = !v
+}
+
+function isAwesome(){
+    return awesome.value
+}
+
 </script>
 
 <template>
-<button @click="awesome = !awesome">toggle</button>
+<button @click="toggleAwesome(awesome)">toggle</button>
 
-    <h1 v-if="awesome">Vue is awesome!</h1>
+    <h1 v-if="isAwesome()">Vue is awesome!</h1>
     <h1 v-else>Oh no 😥 </h1>
 
-    <h4 v-if = "awesome">TRUE</h4>
-    <h4 v-show = "awesome">TRUE</h4>
+    <h4 v-if = "isAwesome">v-if TRUE</h4>
+    <h4 v-show = "isawesome()">v-show TRUE</h4>
 </template>
 
