f39a1c7820d8d04f5a0dc980085b73823faf0c14
qkralsgml78 <qkralsgml78@gmail.com>
2024-02-02 12:43:27 +0900

    VUE Watchers(감시자) 사용

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,32 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
-const show = ref(true)
-const list = ref([1, 2, 3])
+const todoId = ref(1);
+const todoData = ref(null);
 
-const toggleList = () => {
-  show.value = !show.value
+async function fetchData() {
+    todoData.value = null;
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+    );
+    todoData.value = await res.json();
+}
+fetchData();
+
+function get(){
+    todoId.value++;
+    //fetData()
 }
 
-const addToList = () => {
-  list.value.push(Math.max(...list.value) + 1)
-}
+//감시자
+watch(todoId, fetchData);
 
-const removeFromList = () => {
-  list.value.pop()
-}
-
-const reverseList = () => {
-  list.value.reverse()
-}
 </script>
 
 <template>
-  <button @click="toggleList">List 렌더링 ON/OFF </button>
-  <button @click="addToList">List 추가</button>
-  <button @click="removeFromList">List 제거</button>
-  <button @click="reverseList">List 뒤집기</button>
-
-  <ul v-if="show">
-    <li v-for="item in list" :key="item">{{ item }} </li>
-  </ul>
-  <p v-else-if="list.length">List is not empty, but hidden.</p>
-  <p v-else>List is not empty.</p>
+    <p>Todo id: {{ todoId }}</p>
+    <button @click="get" :disabled="!todoData">Fetch next todo</button>
+    <p v-if="!todoData">Loading...</p>
+    <pre v-else>{{ todoData }}</pre>
 </template>
 
